42a5772b594982368facad60cde92c948b400f96
lidia <104456433+lalvionisya@users.noreply.github.com>
2022-12-11 12:37:22 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,4 @@
-if (localStorage.getItem('theme') == 'dark')
-    setTheme(true)
-
-function setTheme(isDark) {
-    if (isDark) {
-        document.body.setAttribute('id', 'darkmode')
-        localStorage.setItem('theme', 'dark')
-    } else {
-        document.body.setAttribute('id', '')
-        localStorage.removeItem('theme')
-    }
+function setTheme() {
+    const ubah = document.body;
+    ubah.classList.toggle('darkmode');
 }
